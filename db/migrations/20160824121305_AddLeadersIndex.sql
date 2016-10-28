09a0fb2672d2e18e1c35854e2d78849717a3c587
manue1 <manuel.bergler@gmail.com>
2016-10-28 13:00:23 -0500

    Goose DB migrations with config file.

--- db/migrations/20160824121305_AddLeadersIndex.sql
+++ db/migrations/20160824121305_AddLeadersIndex.sql
@@ -1,9 +1,17 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-CREATE INDEX leaders_public_footprint_index
-ON users(first_name, last_name, total_footprint, city, state, county)
-WHERE public IS TRUE AND (total_footprint->'result_grand_total') IS NOT NULL;
+-- +goose StatementBegin
+DO $$
+    BEGIN
+      IF to_regclass('public.leaders_public_footprint_index') IS NULL THEN
+        CREATE INDEX leaders_public_footprint_index
+          ON users(first_name, last_name, total_footprint, city, state, county)
+          WHERE public IS TRUE AND (total_footprint->'result_grand_total') IS NOT NULL;
+      END IF;
+    END;
+$$
+-- +goose StatementEnd
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
